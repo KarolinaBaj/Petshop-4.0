29a89af6498f1d3a0914d0930bfb261cded32f05
KarolinaBaj <modifikacija9@gmail.com>
2026-09-03 03:44:42 +0200

    f
    d

---
+++ WebApplication1/Petshop.DAL/PetshopDBcontext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Petshop.DAL
+{
+    public class PetshopDBcontext : DbContext
+    {
+        public PetshopDBcontext(DbContextOptions<PetshopDBcontext> options) : base(options)
+        {
+        }
+        
+    }
+}
